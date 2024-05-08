99d379b83e9ece298c5e0ef2897d470bb16d43c0
r.b4rr10s <rodrigoedu11@gmail.com>
2024-05-08 18:14:12 -0500

    Added available crud option for topologies as a list

--- cmd/topologies.go
+++ cmd/topologies.go
@@ -5,7 +5,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
 )
 
@@ -13,10 +15,108 @@ import (
 var topologiesCmd = &cobra.Command{
 	Use:   "topologies",
 	Short: "Manage CRUD operations related to topologies",
-	Long: `Manage CRUD operations to topologies`,
+	Long:  `Manage CRUD operations to topologies`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("topologies called")
+		p := tea.NewProgram(initialModel())
+		m, err := p.Run()
+		if err != nil {
+			fmt.Printf("Alas, there's been an error: %v", err)
+			os.Exit(1)
+		}
+		if m, ok := m.(model); ok && m.choices[m.cursor] != "" {
+			fmt.Printf("\n---\nYou chose %s!\n", m.choices[m.cursor])
+		}
 	},
+}
+
+type model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func initialModel() model {
+	return model{
+		choices: []string{"Create topology", "List topologies", "Edit topology", "Delete topoly"},
+
+		selected: make(map[int]struct{}),
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	// Just return `nil`, which means "no I/O right now, please."
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+				return m, tea.Quit
+			}
+		}
+	}
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	// The header
+	s := ">Available CRUD topology operations\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "•" // selected!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s (%s) %s\n", cursor, checked, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
 }
 
 func init() {
