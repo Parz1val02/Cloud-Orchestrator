9b431bfa57817e7eda0c41870e507e8b11703c99
CarlosPisco <edupiscote.cept@gmail.com>
2024-05-22 12:38:55 -0500

    generara topologia al recibir POST

--- grafos/withDash.py
+++ grafos/withDash.py
@@ -2,47 +2,20 @@ from dash import Dash, html, dcc, Input, Output, State
 import dash_cytoscape as cyto
 import networkx as nx
 import json
+from flask import request, jsonify
 import requests
 
-
 app = Dash(__name__)
+server = app.server
 
+latest_topology_data = None
 
 app.layout = html.Div([
 
     
 
     html.H1("Network Topology Generator"),
-    
-    
-
-    html.Label("Escoja la topologia a generar:"),
-    dcc.Dropdown(
-        id='topology-type-dropdown',
-        options=[
-            {'label': 'Lineal', 'value': 'Lineal'},
-            {'label': 'Malla', 'value': 'Malla'},
-            {'label': 'Arbol', 'value': 'Arbol'},
-            {'label': 'Anillo', 'value': 'Anillo'},
-            {'label': 'Bus', 'value': 'Bus'}
-        ],
-        value='Escoja una topologia a armar' 
-    ),
-    
-    html.Label("Ingrese el numero de nodos: "),
-    dcc.Input(id='num-nodes-input', type='number', value="Numero de nodos"),
-
-   
-    
-    html.Button('Generate Topology', id='generate-button', n_clicks=0 ),
-    
-    html.Div(id='cytoscape-container'),
-
-    html.Div(id='json-output'),
-    html.Button('Save JSON Data', id='save-button', n_clicks=0)
-    
-    
-
+    html.Div(id='cytoscape-container')
 
 
     
@@ -52,13 +25,19 @@ app.layout = html.Div([
 @app.callback(
     
     Output('cytoscape-container', 'children'),
-    
-    [Input('generate-button', 'n_clicks')],
-    [State('topology-type-dropdown', 'value'),
-     State('num-nodes-input', 'value')]
+     Input('cytoscape-container', 'id')
+   
 )
-def generate_topology(n_clicks, topology_type, num_nodes):
-    if n_clicks > 0:
+def generate_topology(_):
+    #print(json_data)
+    global latest_topology_data
+    if latest_topology_data:
+        data = latest_topology_data
+        topology_type = data.get('topology_type')
+        num_nodes = data.get('num_nodes')
+
+        if topology_type is None or num_nodes is None:
+            return html.Div("Error: Missing topology_type or num_nodes in JSON data")
         G = nx.Graph()
         
         if topology_type == 'Lineal':
@@ -79,10 +58,6 @@ def generate_topology(n_clicks, topology_type, num_nodes):
             edge_list = [(i, i + 1) for i in range(num_nodes - 1)]
             edge_list.append((0, num_nodes - 1))
             G.add_edges_from(edge_list)
-        elif topology_type == 'Bus':
-            for i in range(1, num_nodes + 1):
-                for j in range(i + 1, num_nodes + 1):
-                    G.add_edge(i, j)
         
         elements = [{'data': {'id': str(node), 'label': str(node)}} for node in G.nodes()]#nodos
         elements += [{'data': {'source': str(edge[0]), 'target': str(edge[1])}} for edge in G.edges()]#links
@@ -94,7 +69,7 @@ def generate_topology(n_clicks, topology_type, num_nodes):
             "user_id": "6640550a53c1187a6899a5a9",
             "deployed": False,
             "name": "Template Estrella",
-            "description": "Template con topología tipo estrella",
+            "description": "Template con topologia tipo estrellaaa",
             "vlan_id": "vlan_id_estrella",
             "topology": {
                 "nodes": [],
@@ -155,35 +130,130 @@ def generate_topology(n_clicks, topology_type, num_nodes):
         json_string = json.dumps(json_data, indent=4)
         print(json_string)
         #------
+        
+        #json_string = json.dumps(json_data, indent=4)
 
         return cyto.Cytoscape(
             id='network-graph',
             elements=elements,
             layout={'name': 'grid'},
-            style={'width': '100%', 'height': '600px'}
+            style={'width': '100%', 'height': '500px'}
         )
+    
+        
     else:
         return html.Div()
 
+
+'''
 @app.callback(
     Output('json-output', 'children'),
-    [Input('save-button', 'n_clicks')],
-    [State('json-output', 'children')]  
+    [Input('save-button', 'n_clicks')]
+    
 )
-def save_json(n_clicks, json_data):
+def save_json(n_clicks):
     if n_clicks > 0:
         
-        endpoint = 'http://127.0.0.1:27017/templates/6640688d53c1187a6899a5c5'
-        
-       
-        response = requests.put(endpoint, json=json_data)
+        #json_data = json_string
+
+        json_data = {
+                    "_id": {
+                        "$oid": "6640688d53c1187a6899a5c5"
+                    },
+                    "user_id": "6640550a53c1187a6899a5a9",
+                    "deployed": 0,
+                    "name": "Template Estrella",
+                    "description": "Template con topologia tipo estrellaaa",
+                    "vlan_id": "vlan_id_estrella",
+                    "topology": {
+                        "nodes": [
+                            "0",
+                            "1",
+                            "2"
+                        ],
+                        "links": [
+                            {
+                                "source": "0",
+                                "target": "1"
+                            },
+                            {
+                                "source": "1",
+                                "target": "2"
+                            }
+                        ],
+                        "specifications": {
+                            "central_node": {
+                                "cpu": 4,
+                                "memory": 8,
+                                "storage": 100,
+                                "image": "Ubuntu",
+                                "access_protocol": "SSH",
+                                "security_rules": [
+                                    22
+                                ]
+                            },
+                            "peripheral_node1": {
+                                "cpu": 2,
+                                "memory": 4,
+                                "storage": 50,
+                                "image": "Ubuntu",
+                                "access_protocol": "SSH",
+                                "security_rules": [
+                                    22
+                                ]
+                            },
+                            "peripheral_node2": {
+                                "cpu": 2,
+                                "memory": 4,
+                                "storage": 50,
+                                "image": "Ubuntu",
+                                "access_protocol": "SSH",
+                                "security_rules": [
+                                    22
+                                ]
+                            },
+                            "peripheral_node3": {
+                                "cpu": 2,
+                                "memory": 4,
+                                "storage": 50,
+                                "image": "Ubuntu",
+                                "access_protocol": "SSH",
+                                "security_rules": [
+                                    22
+                                ]
+                            }
+                        }
+                    }
+        }
+
+        endpoint = 'http://127.0.0.1:5000/templates/6640688d53c1187a6899a5c5'
+
+        json_string = json.dumps(json_data, indent=4)
+
+        response = requests.put(endpoint, json=json_string)
         
         if response.status_code == 200:
             return "JSON data saved successfully!"
         else:
+            print(response.text)
             return "Error: Failed to save JSON data"
     else:
         return ""
+'''
+
+@server.route('/api/generate_topology', methods=['POST'])
+def generate_topology_api():
+    global latest_topology_data
+    if request.method == 'POST':
+        data = request.get_json()
+        #print(data)
+        topology_type = data.get('topology_type')
+        num_nodes = data.get('num_nodes')
+        if not topology_type or not num_nodes:
+            return jsonify({"error": "Invalid input"}), 400
+        latest_topology_data = data
+        return jsonify(data), 200
+
 
 
 if __name__ == '__main__':
