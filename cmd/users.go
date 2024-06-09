a0d35ed478a30aa59c70214a09b0754c2b453d4f
r.b4rr10s <rodrigoedu11@gmail.com>
2024-06-09 17:27:34 -0500

    F crud de users

--- cmd/users.go
+++ cmd/users.go
@@ -4,36 +4,36 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+	//"fmt"
 
-	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
+	//"github.com/spf13/cobra"
+	//"github.com/spf13/viper"
 )
 
 // usersCmd represents the users command
-var usersCmd = &cobra.Command{
-	Use:   "users",
-	Short: "Manage CRUD operation related to users",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("users called")
-	},
-}
+//var usersCmd = &cobra.Command{
+//	Use:   "users",
+//	Short: "Manage CRUD operation related to users",
+//	Long: `A longer description that spans multiple lines and likely contains examples
+//and usage of using your command. For example:
+//
+//Cobra is a CLI library for Go that empowers applications.
+//This application is a tool to generate the needed files
+//to quickly create a Cobra application.`,
+//	Run: func(cmd *cobra.Command, args []string) {
+//		fmt.Println("users called")
+//	},
+//}
 
 func init() {
-	initConfig()
-	err := viper.ReadInConfig()
-	if err == nil {
-		role := viper.GetString("role")
-		if role == "administrator" {
-			rootCmd.AddCommand(usersCmd)
-		}
-	}
+	//initConfig()
+	//err := viper.ReadInConfig()
+	//if err == nil {
+	//	role := viper.GetString("role")
+	//	if role == "administrator" {
+	//		rootCmd.AddCommand(usersCmd)
+	//	}
+	//}
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
