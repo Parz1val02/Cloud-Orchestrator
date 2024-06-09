0db4d3308b853b5312bf9b4c8cc84f9136842dc9
CarlosPisco <edupiscote.cept@gmail.com>
2024-06-09 18:26:32 -0500

    borrado en bd

--- modules/getresources2.py
+++ modules/getresources2.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request
 import json
 from pymongo import MongoClient
-
+import asyncio
 
 app = FastAPI()
 
@@ -20,6 +20,42 @@ async def receive_data(request: Request):
     print(f"Received data: {data}")
     result = collection.insert_one(data)
     return {"status": "success"}
+
+
+async def revisar_y_eliminar_excesos():
+    while True:
+        query1 = {"worker1": "10.0.0.30"}
+        documentos1 = list(collection.find(query1).sort("timestamp"))
+        
+        if len(documentos1) > 5:
+            documentos_a_eliminar1 = len(documentos1) - 5
+            for i in range(documentos_a_eliminar1):
+                collection.delete_one({"_id": documentos1[i]["_id"]})
+            print(f"Se han eliminado {documentos_a_eliminar1} documentos antiguos del worker1")
+        
+        query2 = {"worker2": "10.0.0.40"}
+        documentos2 = list(collection.find(query2).sort("timestamp"))
+        
+        if len(documentos2) > 5:
+            documentos_a_eliminar2 = len(documentos2) - 5
+            for i in range(documentos_a_eliminar2):
+                collection.delete_one({"_id": documentos2[i]["_id"]})
+            print(f"Se han eliminado {documentos_a_eliminar2} documentos antiguos del worker2")
+
+
+        query3 = {"worker3": "10.0.0.50"}
+        documentos3 = list(collection.find(query3).sort("timestamp"))
+        
+        if len(documentos3) > 5:
+            documentos_a_eliminar3 = len(documentos3) - 5
+            for i in range(documentos_a_eliminar3):
+                collection.delete_one({"_id": documentos3[i]["_id"]})
+            print(f"Se han eliminado {documentos_a_eliminar3} documentos antiguos del worker2")
+
+        await asyncio.sleep(5)  # Espera 5 segundos antes de revisar nuevamente
+
+async def app_lifespan():
+    await revisar_y_eliminar_excesos()
 
 if __name__ == "__main__":
     import uvicorn
