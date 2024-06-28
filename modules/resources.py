b18a17bd686c06b4c74e8a9b0e90caf86707b3a9
CarlosPisco <108200343+CarlosPisco@users.noreply.github.com>
2024-06-28 14:43:12 -0500

    Update resources.py
    now 8 cores

--- modules/resources.py
+++ modules/resources.py
@@ -1,3 +1,4 @@
+
 import socket
 import time
 import subprocess
@@ -10,8 +11,6 @@ import requests
 utilizacionCPU = []
 infoMemoria = []
 infoStorage = []
-velocidadTX = []
-velocidadRX = []
 
 
 def envioInformacion(info):
@@ -23,29 +22,37 @@ def envioInformacion(info):
 
 def findCPU():
     global utilizacionCPU
-    command_CPU = "initial=$(cat /proc/stat | grep cpu | awk '{print $5}'); echo $initial; sleep 3; final=$(cat /proc/stat | grep cpu | awk '{print $5}'); echo $final"
+    command_CPU = "initial=$(cat /proc/stat | grep cpu | awk '{print $5}'); echo $initial; sleep 0.5; final=$(cat /proc/stat | grep cpu | awk '{print $5}'); echo $final"
     procces_CPU = subprocess.Popen(command_CPU, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = procces_CPU.communicate()
     output = output.decode('utf-8')
-    size = int(len(output.replace("\n"," ").strip(" ").split(" ")))
-    informacionCPU1 = output.replace("\n"," ").strip(" ").split(" ")[:int(size/2)]
-    informacionCPU2 = output.replace("\n"," ").strip(" ").split(" ")[int(size/2):]
-    # CPU 0 y CPU 1
+    lsIDLE =  output.replace("\n"," ").strip(" ").split(" ")
+    lsIDLE.pop(0)
+    lsIDLE.pop(8)
+    size = len(lsIDLE)
+    mitad = size//2
+    parte1 = lsIDLE[:mitad]
+    parte2 = lsIDLE[mitad:]
+    print(parte1)
+    print(parte2)
+    #informacionCPU1 = [lsIDLE[1],lsIDLE[10]]
     aux = []
-    for i in range(1,int(size/2)):
-        delta = int(informacionCPU2[i]) - int(informacionCPU1[i])
-        #Tener en cuenta que en 3 s hay 300 jiffys -> esto se podría hacer de manera dinámica
-        cpu_value = ((300-delta)/300)*100
-        aux.append(cpu_value)
+    for i in range(size//2):
+        #encontramos los porcetnajes de CPU para los 0.5 seg examinados
+        delta =  int(parte2[i]) - int(parte1[i])
+		diff = 50 - delta
+		if diff < 0:
+			diff=0
+		cpu_used = (diff/50)*100
+        aux.append(cpu_used)
     utilizacionCPU = aux
-    print("Informacion del CPU recolectada correctamente")
-
+    print("Informacion de CPU recolectada correctamente")
 
 #Info de la memoria RAM
 #"MemoriaUsada(Gb)": , "MemoriaDisponible(Mb)": , "MemoriaTotal(Gb)":
 def findMemory() :
     global infoMemoria
-    command_Memory = "free -b | awk '/^Mem:/{printf \"%.1f GB , %.1f MB , %.1f GB\\n\", $2/1000000000, $3/1000000, $7/1000000000}'"
+    command_Memory = "free -b | awk '/^Mem:/{printf \"%.1f MB , %.1f MB , %.1f MB\\n\", $2/1000000, $3/1000000, $7/1000000}'"
     process_Memory = subprocess.Popen(command_Memory, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output, error = process_Memory.communicate()
     output = output.decode('utf-8')
@@ -58,35 +65,22 @@ def findMemory() :
 # "AlmacenamientoUsado(Gb)": , "AlmacenamientoUsado(%)": , "AlmacenamientoTotal(Gb)":
 def findStorage():
     global infoStorage
-    numbers = ["9","10","11"]
-
-    for number in numbers:
-        try:
-            output = findLineStorage(number)
-            print(number+" "+output)
-            aux = output.replace("\n"," ").replace("   "," ").strip(" ").split(" ")
-            if 'G' in aux[0]:
-                infoStorage=aux
-        except:
-            pass
-
-
-    '''global infoStorage
-
-
     try:
-        output = findLineStorage(11)
-        print(output)
-        aux = output.replace("\n"," ").replace("   "," ").strip(" ").split(" ")
-        if 'G' in aux[0]:
-            infoStorage=aux
-    except:
-        pass'''
+       output = findLineStorage("11")
+       aux = output.replace("\n"," ").replace("   "," ").strip(" ").split(" ")
+       if 'G' in aux[0]:
+           infoStorage = aux
+       else:
+           infostorage = []
+    except Exception as e:
+       print(f"Error processing line 11: {e}")
+       infoStorage = []
+
 
 
     print("Informacion del almacenamiento recolectada correctamente")
-
-
+    print(infoStorage)
+	
 def findLineStorage(lineNumber):
     command_Storage = "lsblk -o FSSIZE,FSUSED,FSUSE% | sed -n '"+lineNumber+"p'"
     process_Storage = subprocess.Popen(command_Storage, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -95,29 +89,6 @@ def findLineStorage(lineNumber):
     return output
 
 
-# "ens3(RX)bps": , "ens3(TX)bps": , "ens4(RX)bps": , "ens4(TX)bps":
-def findBandWith():
-    global velocidadRX
-    global velocidadTX
-    #                                   extraer bits recibidos y transmitidos con un margen de 3 segundos
-    command_Bandwith = "cat /proc/net/dev | grep -E 'ens3|ens4' | awk '{print $2, $10}'; sleep 3;cat /proc/net/dev | grep -E 'ens3|ens4' | awk '{print $2, $10}'"
-    process_Bandwith = subprocess.Popen(command_Bandwith, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process_Bandwith.communicate()
-    output = output.decode('utf-8')
-    size = int(len(output.replace("\n"," ").strip(" ").split(" ")))
-    infoRed1 = [int(valor) for valor in output.replace("\n"," ").strip(" ").split(" ")[:int(size/2)]] #pertenecientes a ens3
-    infoRed2 = [int(valor) for valor in output.replace("\n"," ").strip(" ").split(" ")[int(size/2):]] #pertenecientes a ens4
-    auxTX = []
-    auxRX = []
-    for i in range(0,int(size/2)):
-        delta = infoRed2[i] - infoRed1[i]
-        if i%2 == 0:
-            auxTX.append(round(float(delta/3.0),1))
-        else:
-            auxRX.append(round(float(delta/3.0),1))
-    velocidadRX = auxRX
-    velocidadTX = auxTX
-    print("Informacion de red recolectada correctamente")
 
 if __name__ == "__main__":
     while True:
@@ -126,46 +97,35 @@ if __name__ == "__main__":
         hilo_CPU = threading.Thread(target=findCPU)
         hilo_memoria = threading.Thread(target=findMemory)
         hilo_storage = threading.Thread(target=findStorage)
-        hilo_TX = threading.Thread(target=findBandWith)
-        hilos = [hilo_CPU,hilo_memoria,hilo_storage,hilo_TX]
+        #hilo_TX = threading.Thread(target=findBandWith)
+        hilos = [hilo_CPU,hilo_memoria,hilo_storage]
         for hilo in hilos:
             hilo.start()
         for hilo in hilos:
             hilo.join()
-        #Aca debería tener la informacion ya recolectada
+        #Ordenamos la data recopilada para enviarla
         overallInfo = {}
+        overallInfo["worker1"] =  "10.0.0.30"
         #CPU
         for i in range(len(utilizacionCPU)):
             overallInfo["Core"+str(i)+"(%)"] = round(utilizacionCPU[i],1)
 
-        core_keys = [key for key in overallInfo.keys() if key.startswith("Core")]
-        total_cpu_usage = sum(overallInfo[key] for key in core_keys)
-        average_cpu_usage = total_cpu_usage / len(core_keys)
-
-        overallInfo["average_cpu_usage (%)"] = average_cpu_usage
-
         #Memoria
-        overallInfo["MemoriaUsada(Gb)"]= float(infoMemoria[6]) if float(infoMemoria[3])>float(infoMemoria[6]) else   float(infoMemoria[3])
-        overallInfo["MemoriaDisponible(Mb)"]= float(infoMemoria[3]) if float(infoMemoria[3])>float(infoMemoria[6]) else   float(infoMemoria[6])
-        overallInfo["MemoriaTotal(Gb)"]=float(infoMemoria[0])
+        #print(infoMemoria)
+        overallInfo["MemoriaUsada(Mb)"]= float(infoMemoria[3])
+        overallInfo["MemoriaDisponible(Mb)"]= float(infoMemoria[6])
+        overallInfo["MemoriaTotal(Mb)"]=float(infoMemoria[0])
         #Almacenamiento
-        print(infoStorage)
+        #print(infoStorage)
         overallInfo["AlmacenamientoUsado(Gb)"]=float(infoStorage[1].strip("G"))
         overallInfo["AlmacenamientoUsado(%)"]=int(infoStorage[3].strip("%"))
         overallInfo["AlmacenamientoTotal(Gb)"]=float(infoStorage[0].strip("G"))
-        #Red
-        overallInfo["ens3(RX)bps"]=velocidadRX[0]
-        overallInfo["ens3(TX)bps"]=velocidadTX[0]
-        overallInfo["ens4(RX)bps"]=velocidadRX[1]
-        overallInfo["ens4(TX)bps"]=velocidadTX[1]
         #Timestamp
         overallInfo["timestamp"]=datetime.now().strftime("%d-%m-%Y %H:%M:%S")
 
         print("Informacion recolectada correctamente")
         print("Enviando la información al servidor")
-        #envioInformacion(json.dumps(overallInfo))
-        print("Informacion enviada correctamente!")
         #print(overallInfo)
-        print(json.dumps(overallInfo))
+		print(json.dumps(overallInfo))
         envioInformacion(json.dumps(overallInfo))
-        time.sleep(5)
+        time.sleep(0.5)
