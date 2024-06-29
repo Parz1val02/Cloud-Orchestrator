8ebdb09e88f03bfc562a816f9642cb05c5071e5f
xTarantino28 <gzanabriaz28@gmail.com>
2024-06-29 12:45:09 -0500

    sliceservice proxy

--- api-gateway/main.go
+++ api-gateway/main.go
@@ -15,6 +15,7 @@ import (
 const (
 	APIGatewayURL      = "http://localhost:4444"
 	TemplateserviceURL = "http://localhost:5000"
+	SliceserviceURL    = "http://localhost:9999"
 )
 
 func main() {
@@ -23,14 +24,18 @@ func main() {
 		log.Fatalf("Error creating reverse proxy for templateservice: %v", err2)
 	}
 
-	r := gin.Default()
+	proxySliceManager, errSliceManager := createReverseProxySliceManager(SliceserviceURL)
+	if errSliceManager != nil {
+		log.Fatalf("Error creating reverse proxy for sliceservice: %v", errSliceManager)
+	}
 
+	r := gin.Default()
 	r.Use(authenticate())
 	r.Use(rateLimit())
-
 	r.POST("/login", loginHandler)
 	// r.POST("/logout", logoutHandler)
 	r.Any("/templateservice/*path", proxy2)
+	r.Any("/sliceservice/*path", proxySliceManager)
 
 	log.Printf("API Gateway listening on %s", APIGatewayURL)
 	log.Fatal(r.Run(":4444"))
@@ -48,6 +53,29 @@ func createReverseProxy(targetURL string) (func(*gin.Context), error) {
 		log.Printf("Request received for %s", c.Request.URL.Path)
 
 		c.Request.URL.Path = strings.TrimPrefix(c.Request.URL.Path, "/templateservice")
+		if user, exists := c.Get("user"); exists {
+			userInfo := user.(UserResponse)
+			c.Request.Header.Set("X-User-ID", userInfo.ID)
+			c.Request.Header.Set("X-User-Username", userInfo.Username)
+			c.Request.Header.Set("X-User-Role", userInfo.Role)
+		}
+
+		proxy.ServeHTTP(c.Writer, c.Request)
+	}, nil
+}
+
+func createReverseProxySliceManager(targetURL string) (func(*gin.Context), error) {
+	target, err := url.Parse(targetURL)
+	if err != nil {
+		return nil, err
+	}
+
+	proxy := httputil.NewSingleHostReverseProxy(target)
+
+	return func(c *gin.Context) {
+		log.Printf("Request received for %s", c.Request.URL.Path)
+
+		c.Request.URL.Path = strings.TrimPrefix(c.Request.URL.Path, "/sliceservice")
 		if user, exists := c.Get("user"); exists {
 			userInfo := user.(UserResponse)
 			c.Request.Header.Set("X-User-ID", userInfo.ID)
